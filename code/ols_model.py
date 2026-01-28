f0a1b5479fb6ad7fe5fd2949d3056f7e5d599e63
anteabendo <antea.bendo@fti.edu.al>
2026-01-28 20:05:52 +0100

    docker provision for learningBase and activationBase

--- code/ols_model.py
+++ code/ols_model.py
@@ -1,50 +1,65 @@
-import statsmodels.api as sm
 import pandas as pd
+import statsmodels.api as sm
 import matplotlib.pyplot as plt
-import numpy as np
+import diagnostics_plots as dp
 from sklearn.metrics import mean_squared_error, r2_score
 
 #load data
 data = pd.read_csv("../data/training_data.csv")
-training = data['time'].values
-y_train = data['price'].values
+training_value_x = data[['time', 'price_lag']].values
+training_value_y = data['target_price'].values
 
 testing_data = pd.read_csv("../data/testing_data.csv")
-testing = testing_data['time'].values
-y_test = testing_data['price'].values
+testing_value_x = testing_data[['time', 'price_lag']].values
+testing_value_y = testing_data['target_price'].values
 
-x_training = sm.add_constant(training)
+x_training = sm.add_constant(training_value_x)
 
 #ols model
-ols_model = sm.OLS(y_train, x_training).fit()
+ols_model = sm.OLS(training_value_y, x_training).fit()
 print(ols_model.summary())
 
 #save model
-ols_model.save("./currentOlsSolution.xml")
+ols_model.save("../images/knowledgeBase_energy_prediction/currentOlsSolution.xml")
 
 #predictions
-x_testing = sm.add_constant(testing)
+x_testing = sm.add_constant(testing_value_x)
 y_pred = ols_model.predict(x_testing)
 
-print(f"Mean squared error: {mean_squared_error(y_test, y_pred):.2f}")
-print(f"Coefficient of determination: {r2_score(y_test, y_pred):.2f}")
+print(f"Mean squared error: {mean_squared_error(testing_value_y, y_pred):.2f}")
+print(f"Coefficient of determination: {r2_score(testing_value_y, y_pred):.2f}")
 
 #scatterplot
-plt.scatter(training, y_train, color='orange', label='Training Data', alpha=0.7)
-plt.scatter(testing, y_test, color='blue', label='Testing Data')
+plt.figure(figsize=(14,5))
+plt.scatter(training_value_x[:, 0], training_value_y, color='orange', label='Training Data', alpha=0.7)
+plt.scatter(testing_value_x[:, 0], testing_value_y, color='blue', label='Testing Data')
 
-fig = sm.graphics.abline_plot(model_results=ols_model, color='red', label="OLS regression", ax=plt.gca())
+plt.plot(testing_value_x[:, 0], y_pred, color='red', label='OLS Prediction Trend', linewidth=2)
 
 plt.xlabel('Time')
 plt.ylabel('Price')
 plt.title('OLS Regression Fit')
 plt.legend()
+plt.tight_layout()
+plt.savefig('scatterplot-time.png')
+
+#scatterplot
+plt.figure(figsize=(14,5))
+plt.scatter(training_value_x[:, 1], training_value_y, color='orange', label='Training Data', alpha=0.7)
+plt.scatter(testing_value_x[:, 1], testing_value_y, color='blue', label='Testing Data')
+
+#fig = sm.graphics.abline_plot(model_results=ols_model, color='red', label="OLS regression", ax=plt.gca())
+plt.plot(testing_value_x[:, 1], y_pred, color='red', label='OLS Prediction Trend', linewidth=2)
+
+plt.xlabel('Price Lag')
+plt.ylabel('Price')
+plt.title('OLS Regression Fit')
+plt.legend()
 #plt.show()
 plt.tight_layout()
-plt.savefig('scatterplot.pdf')
+plt.savefig('scatterplot-lag.png')
 
 #diagnostics
-import diagnostics_plots as dp
 cls = dp.Linear_Reg_Diagnostic(ols_model)
 cls(plot_context="seaborn-v0_8-paper")
 
